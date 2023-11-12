9bc7ccbb34b4a55fe36db05043b76fc6c99ee667
Sabrina <sabsabrinacheung@gmail.com>
2023-11-12 20:17:32 +0800

    test page

--- src/App.js
+++ src/App.js
@@ -4,16 +4,23 @@ import { MsalProvider } from "@azure/msal-react";
 import React, { useEffect, useState } from 'react';
 import Protected from './Protected';
 import Emails from './Emails';
-import Pages from './Pages';
-const App = () => {
-
-  const { emails, setEmails } = useState(null);
-  const { page, setPage } = useState(1);
 
 
-  function getAllEmails(pageInt = page){
-    
-    fetch("http://127.0.0.1:5000/emails/?page=" + pageInt, {
+const App = ({ msalInstance})=> {
+  const [emails, setEmails] = useState(null);
+  const [page, setPage] = useState(1);
+
+  function handlePage(pageInt) {
+    setPage(pageInt);
+    getAllEmails();
+  }
+
+  function handleEmails(emails) {
+    setEmails(emails);
+  }
+
+  function getAllEmails() {
+    fetch("http://127.0.0.1:5000/emails/?page=" + page, {
       method: 'GET',
       headers: {
         'Content-Type': 'application/json',
@@ -22,40 +29,38 @@ const App = () => {
     }).then(res => res.json())
       .then((resJson) => {
         const data = JSON.parse(resJson).emails;
-        setEmails(data);
+        handleEmails(data);
     }).catch(error => {
       console.log(error);
     })
   };
-  
-  return (
-    <div>
-      {emails === null &&  
-        <div>
-          <h1>Welcome to My App</h1>
-          <LoginButton />
-        </div>
-      }
 
-      {emails !== null &&
-        <>
-        <LoginButton />
-        <Emails emails={emails} />
-        <Pages pageNum={ 5} setPage={setPage}/>
-        </>
-        }
-      
-    </div>
+  const Pages = ({ pageNum }) => {
+  var pages = [];
+  var page = -1;
+  pageNum === null ? page = 5 : page = pageNum;
 
-  );
+  function addPages(pageInt) {
+    return <button onClick={(e) => handlePage(e.target.value)}>
+            {pageInt}
+    </button>;
+  }
+
+  for (var i = 1; i <= page; i++) {
+    pages.push(addPages(i));
+  }
+  return <div className="pageRow" >{pages}</div>;
 };
 
-const AppWithMsal = ({ msalInstance }) => {
   return (
-    <MsalProvider instance={msalInstance}>
-      <App />
+    <MsalProvider instance={ msalInstance}>
+      <>
+        <LoginButton />
+        <Emails emails={emails} />
+        <Pages pageNum={5} getAllEmails={getAllEmails} />
+      </>
     </MsalProvider>
   );
 };
 
-export default AppWithMsal;
+export default App;
